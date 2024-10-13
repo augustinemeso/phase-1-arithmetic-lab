9f39a9d16c9044e5fc8aa665e37aa46a6d611f6f
augustinemeso <augustinemesobk@gmail.com>
2024-10-13 20:31:39 +0300

    add phase-1-arithmetic-lab

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+// 1. Create a variable called multiply set to an equation that will multiply the variables num1 and num2; the result of the multiplication should be 62.
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2; // Result should be 62
 
-// Write your code here
+// 2. Create a variable called random that will generate a random integer greater than 0.
+let random = Math.floor(Math.random() * 100) + 1; // Generates a random integer between 1 and 100
+
+// 3. Create a variable called mod set to an equation that will calculate the remainder of dividing variable num3 by num4; the remainder should be 4.
+let num3 = 28;
+let num4 = 6;
+let mod = num3 % num4; // Result should be 4
+
+// 4. Create a variable called max that finds the highest number in a set; the value returned should be 20.
+let max = Math.max(5, 10, 15, 20); // Finds the highest number in the set
